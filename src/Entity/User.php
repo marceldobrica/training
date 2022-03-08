bdd6e75937447713cccac93b7a7ae1f76341ea0c
Marcel Dobrica <marcel@helpmanager.ro>
2022-03-08 12:14:43 +0200

    Created entities and deleted .gitignore from folders (#6)
    * Created entities and deleted .gitignore from folders
    
    * Solved Rares Moldovan observations
    
    * Updated for consistency and collection never null
    
    * Modified methods - set/get/Programs to set/get/Programmes
    
    * Added description in Programme

---
+++ src/Entity/User.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\Collection;
+
+class User
+{
+    private int $id;
+
+    private string $password;
+
+    private Collection $roles;
+
+    public string $cnp = '';
+
+    public string $firstName = '';
+
+    public string $lastName = '';
+
+    private Collection $programmes;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getRoles(): Collection
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(Collection $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function getProgrammes(): Collection
+    {
+        return $this->programmes;
+    }
+
+    public function setProgrammes(Collection $programmes): self
+    {
+        $this->programmes = $programmes;
+
+        return $this;
+    }
+}
