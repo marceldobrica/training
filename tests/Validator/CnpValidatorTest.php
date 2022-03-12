761617b2276e836ef6f1ea183322979c5322a00e
Marcel Dobrica <marcel@helpmanager.ro>
2022-03-12 16:27:03 +0200

    Deleted Calin Bolea's code and added some tests

--- tests/Validator/CnpValidatorTest.php
+++ tests/Validator/CnpValidatorTest.php
@@ -6,6 +6,7 @@ namespace App\Tests\Validator;
 
 use App\Validator\Cnp;
 use App\Validator\CnpValidator;
+use Symfony\Component\Validator\ConstraintValidatorInterface;
 use Symfony\Component\Validator\Test\ConstraintValidatorTestCase;
 
 class CnpValidatorTest extends ConstraintValidatorTestCase
@@ -13,14 +14,42 @@ class CnpValidatorTest extends ConstraintValidatorTestCase
     public function testCnpLength(): void
     {
         $cnp = '1234';
-        $result = $this->validator->validate($cnp, new Cnp());
+        $this->validator->validate($cnp, new Cnp());
 
         $this->buildViolation('This is not a valid CNP')->assertRaised();
-        //$this->assertNoViolation();
     }
 
-    protected function createValidator()
+    protected function createValidator(): ConstraintValidatorInterface
     {
         return new CnpValidator();
+    }
+
+    public function provideCnp(): array
+    {
+        return [
+            [ '0', false],
+            [ '101', false],
+            [ '1660713034972', true],
+        ];
+    }
+
+    /**
+     * @dataProvider provideCnp
+     */
+    public function testCnp(string $cnp, bool $expectedValid): void
+    {
+        $this->validator->validate($cnp, new Cnp());
+        if ($expectedValid) {
+            $this->assertNoViolation();
+        } else {
+            $this->buildViolation('This is not a valid CNP')->assertRaised();
+        }
+    }
+
+    public function testValidCnp(): void
+    {
+        $cnp = '1660713034972';
+        $this->validator->validate($cnp, new Cnp());
+        $this->assertNoViolation();
     }
 }
