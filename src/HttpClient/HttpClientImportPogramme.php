b4f9ca5e7c1c8486477cc77f74c2baefc0f89b49
Marcel Dobrica <marcel@helpmanager.ro>
2022-03-23 10:04:12 +0200

    Solved Issue #33

--- src/HttpClient/HttpClientImportPogramme.php
+++ src/HttpClient/HttpClientImportPogramme.php
@@ -4,26 +4,39 @@ declare(strict_types=1);
 
 namespace App\HttpClient;
 
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class HttpClientImportPogramme
 {
-    public string $http_address = 'http://evozon-internship-data-wh.herokuapp.com/api/sport-programs';
-
-    public string $http_protocol = 'GET';
+    public const HTTP_PROTOCOL = 'GET';
 
     private HttpClientInterface $client;
 
-    public function __construct(HttpClientInterface $client)
+    private string $httpImportAddress;
+
+    public function __construct(HttpClientInterface $client, string $httpImportAddress)
     {
         $this->client = $client;
+        $this->httpImportAddress = $httpImportAddress;
     }
 
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
     public function fetchData(): array
     {
         $response = $this->client->request(
-            $this->http_protocol,
-            $this->http_address
+            $this::HTTP_PROTOCOL,
+            $this->httpImportAddress
         );
         $content = $response->toArray();
 
