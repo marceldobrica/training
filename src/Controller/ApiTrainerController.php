ae1faad20a46e15fbe027ac65cecb2abf46f0057
Marcel Dobrica <marcel@helpmanager.ro>
2022-03-26 15:53:56 +0200

    Complete feature with test controllers for different routes. May have some problems after upgrade - guard component deprecation

---
+++ src/Controller/ApiTrainerController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiTrainerController
+{
+    /**
+     * @Route("/api/trainer", methods={"GET"})
+     */
+    public function index(): Response
+    {
+        return new JsonResponse([
+            'message' => 'Welcome to your new controller available only for users with ROLE_TRAINER!'
+        ]);
+    }
+}
