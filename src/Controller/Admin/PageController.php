26b6991cf9143eca3b2a3fca9623f2a13b6aefe3
Marcel Dobrica <marcel@helpmanager.ro>
2022-04-06 17:40:13 +0300

    Solved observations

--- src/Controller/Admin/PageController.php
+++ src/Controller/Admin/PageController.php
@@ -11,25 +11,11 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class PageController extends AbstractController
 {
-    private UserRepository $userRepository;
-
-    private Security $security;
-
-    public function __construct(UserRepository $userRepository, Security $security)
-    {
-        $this->userRepository = $userRepository;
-        $this->security = $security;
-    }
-
     /**
      * @Route("/admin", name="app_admin")
      */
     public function showUserAction(AuthenticationUtils $authenticationUtils): Response
     {
-        $user = $this->security->getUser();
-
-        return $this->render('admin/admin_page/index.html.twig', [
-            'user' => $user,
-        ]);
+        return $this->render('admin/admin_page/index.html.twig', []);
     }
 }
