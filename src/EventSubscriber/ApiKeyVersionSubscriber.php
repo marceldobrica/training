6de60d358c29f8942d0d0a9422927f6fed34a2b1
Marcel Dobrica <marcel@helpmanager.ro>
2022-04-13 12:25:06 +0300

    Add X-API-VERSION to headers

---
+++ src/EventSubscriber/ApiKeyVersionSubscriber.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+
+class ApiKeyVersionSubscriber implements EventSubscriberInterface
+{
+    private string $apiVersionKey;
+
+    public function __construct(string $apiVersionKey) {
+        $this->apiVersionKey = $apiVersionKey;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            RequestEvent::class => ['addApiVersionKey', 257]
+        ];
+    }
+
+    public function addApiVersionKey(RequestEvent $event): void
+    {
+        $event->getRequest()->headers->set('X-API-VERSION', $this->apiVersionKey);
+    }
+}
