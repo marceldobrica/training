bfc5e6ab71becd00b7f0be01501fd93db0ff56b7
Marcel Dobrica <marcel@helpmanager.ro>
2022-04-16 12:07:13 +0300

    Added variable in .env

--- src/HttpClient/HttpClientSmsUser.php
+++ src/HttpClient/HttpClientSmsUser.php
@@ -6,8 +6,6 @@ namespace App\HttpClient;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
